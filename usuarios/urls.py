a858406c964e50f300c74e24bf78a675b5a7881f
Lucas Lima <15lucas.lima15@gmail.com>
2022-07-01 18:18:26 -0500

    login/logout concluido

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastrar_tutor/', views.cadastrar_tutor, name="cadastrar_tutor"),
     path('login/', views.login, name="login"),  
     path('logout/', views.logout, name="logout") 
 ]
