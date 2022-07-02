21d037cb81d31b3753d1edf36d1bed0ffe0570a0
Lucas Lima <15lucas.lima15@gmail.com>
2022-07-02 14:04:11 -0500

    Enter

--- usuarios/apps.py
+++ usuarios/apps.py
@@ -5,5 +5,3 @@ class UsuariosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'usuarios'
 
-    def ready(self):
-        import usuarios.signals
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,37 +1,15 @@
 from django.contrib import auth
-from django.forms import PasswordInput
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from rolepermissions.decorators import has_permission_decorator
 
-from .models import Users
-
 # Create your views here.
-
-@has_permission_decorator('cadastrar_tutor')
-def cadastrar_tutor(request):
-    if request.method == "GET":
-        return render(request, 'cadastrar_tutor.html')
-    if request.method == "POST":
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-
-        user = Users.objects.filter(email=email)
-
-        if user.exists():
-            #TODO: utilizar messages do Django
-            return HttpResponse('Email já existe.')
-        
-        user = Users.objects.create_user(username=email, email=email, password=senha, funcao="T")
-
-        #TODO: Redirecionar com message
-        return HttpResponse('Conta Criada')
 
 def login(request):
     if request.method == "GET":
         if request.user.is_authenticated:
-            return redirect(reverse('cadastrar_tutor.html'))
+            return redirect('/')
         return render(request, 'login.html')
     elif request.method == "POST":
         login = request.POST.get('email')
@@ -45,7 +23,7 @@ def login(request):
             return HttpResponse('Usuário inválido')
         
         auth.login(request, user)
-        return render(request, 'base.html')
+        return redirect('/')
 
 def logout(request):
     request.session.flush()
