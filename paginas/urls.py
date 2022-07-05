2c0a1ef59b3e70b79d0b4e0780b017ea670d73f0
Lucas Lima <57276011+LucasUfac@users.noreply.github.com>
2022-07-05 11:13:42 -0500

    Organização dos layouts

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, ModeloView
 
 urlpatterns = [
     # path('endereço', MinhaView.as_view(), name='nome-da-url')
     path('', IndexView.as_view(), name='inicio'),
+    path('modelo/', ModeloView.as_view(), name='modelo'),
+
     # path('visita/', VisitaView.as_view(), name='visita'),
 
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -6,5 +6,9 @@ from django.views.generic import TemplateView
 #     template_name = 'paginas/NomedaPagina.html'
 
 class IndexView(TemplateView):
-    template_name = 'paginas/visita.html' #Pagina de visitantes(usuario deslogado)
+    template_name = 'paginas/Coordenador/CoordenadorInicio.html' #Pagina de visitantes(usuario deslogado)
+
+class ModeloView(TemplateView):
+    template_name = 'paginas/modelo.html' #Pagina de visitantes(usuario deslogado)
+
 
