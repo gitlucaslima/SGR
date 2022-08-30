2d3247a8eb502918b2c6a7b99c464deee3d246a3
j.leandro1998silva@gmail.com <j.leandro1998silva@gmail.com>
2022-08-30 14:26:27 -0500

    js de relatório

--- static/js/validaCampoData.js
+++ static/js/validaCampoData.js
@@ -1,10 +1,46 @@
 
 
-const selectMesRelatorio = document.querySelector(".select-mes-relatorio");
-const inputDateLimite = document.querySelector(".input-date-limite");
+const selectMesRelatorio = document.querySelectorAll(".select-mes-relatorio");
+const inputDateLimite = document.querySelectorAll(".input-date-limite");
+
+const selectMesRelatorioEdit = document.querySelectorAll(".select-edit");
+const inputDateLimiteEdit = document.querySelectorAll(".input-edit");
+
+function redefinirMinDate(itemInputDate,selectMes){
+
+    let ano = itemInputDate.min.split("-")[0]
+    let mes = selectMes.value.length == 2 ? selectMes.value : "0" + selectMes.value
+    let dia = itemInputDate.min.split("-")[2]
+    
+    data = `${ano}-${mes}-${dia}`
+    itemInputDate.min = data
+}
+
+selectMesRelatorio.forEach(item => {
+
+    item.addEventListener("click", () => {
+       
+        inputDateLimite.forEach(itemInput => {
+          
+            itemInput.disabled = false;
+           
+            redefinirMinDate(itemInput,item)
+
+    })   
+
+})})
 
 
-selectMesRelatorio.addEventListener("change",()=>{
+selectMesRelatorioEdit.forEach(item => {
 
-    // inputDateLimite.min = `${}`
-})
+    item.addEventListener("click", () => {
+       
+        inputDateLimiteEdit.forEach(itemInput => {
+            
+          
+            itemInput.disabled = false;
+
+            redefinirMinDate(itemInput,item)    
+
+    })
+})})
