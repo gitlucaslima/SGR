9d3da35a92faa0c1a8dedebc9bda4f93c6e6afb0
j.leandro1998silva@gmail.com <j.leandro1998silva@gmail.com>
2022-09-16 20:44:51 -0500

    make it better

--- sgr/settings.py
+++ sgr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 
+import os
 from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,13 +27,14 @@ SECRET_KEY = 'django-insecure-h=k)!s!0u)(co-80_55=8!*lbd3d+okouj6#5ma4vkc$(6%2-c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://sgrweb.herokuapp.com/"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -120,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
